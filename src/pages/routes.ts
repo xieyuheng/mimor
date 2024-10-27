94d47aa919a00d4de9d220bd068f9fe51e386f48
Xie Yuheng <xyheme@gmail.com>
2024-10-28 01:48:16 +0800

    remove play/

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -3,10 +3,8 @@ import CreateMimor from './create-mimor/CreateMimor.vue'
 import PageNotFound from './errors/PageNotFound.vue'
 import MimorEntrance from './mimor-entrance/MimorEntrance.vue'
 import Mimor from './mimor/Mimor.vue'
-import Play from './play/Play.vue'
 
 export const routes: Array<RouteRecordRaw> = [
-  { path: '/play', component: Play },
   { path: '/mimors', component: MimorEntrance },
   { path: '/mimors/', component: MimorEntrance },
   { path: '/mimors/:src(.*)', component: Mimor },
