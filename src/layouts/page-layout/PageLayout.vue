2a27f37a8e420004b5b902eb8c7e7c486e26addf
Xie Yuheng <xyheme@gmail.com>
2024-10-29 01:28:57 +0800

    up

--- src/layouts/page-layout/PageLayout.vue
+++ src/layouts/page-layout/PageLayout.vue
@@ -4,20 +4,16 @@ import PageHead from './PageHead.vue'
 </script>
 
 <template>
-  <div>
-    <div class="fixed left-0 top-0 z-10 w-full sm:block">
+  <div class="bg-white dark:bg-stone-900 dark:text-white">
+    <div class="fixed left-0 top-0 z-10 w-full">
       <div
-        class="flex h-16 items-center border-b border-stone-600 dark:border-stone-400"
+        class="flex h-12 items-center border-b border-stone-600 sm:h-16 dark:border-stone-400"
       >
-        <PageHead
-          class="bg-white px-3 shadow-sm sm:px-6 dark:bg-stone-900 dark:text-white"
-        />
+        <PageHead class="px-3 shadow-sm sm:px-6" />
       </div>
     </div>
 
-    <div
-      class="h-screen-dynamic flex flex-col pt-12 sm:pt-16 dark:bg-stone-900 dark:text-white"
-    >
+    <div class="h-screen-dynamic flex flex-col pt-12 sm:pt-16">
       <div class="flex h-full w-full flex-col overflow-auto">
         <slot />
       </div>
