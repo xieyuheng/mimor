cb58a5cb46b42e289a851c2ac3b4ae6352a87dc7
Xie Yuheng <xyheme@gmail.com>
2024-10-28 16:17:32 +0800

    up

--- vite-lib.config.mjs
+++ vite-lib.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   build: {
     sourcemap: true,
     outDir: 'lib',
@@ -12,5 +13,4 @@ export default defineConfig({
       fileName: 'index',
     },
   },
-  plugins: [vue()],
 })
