979900732e225ece6af30b986a73fe5d3a27ca3e
Xie Yuheng <xyheme@gmail.com>
2024-10-29 14:42:26 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createHead } from '@vueuse/head'
 import { createApp } from 'vue'
 import App from './App.vue'
-import { Mimor } from './index'
+import './index'
 import router from './router'
 import './styles/index.css'
 
@@ -9,7 +9,5 @@ const app = createApp(App)
 
 app.use(router)
 app.use(createHead())
-
-customElements.define('x-mimor', Mimor)
 
 app.mount('#app')
