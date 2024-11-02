35a65c81846d7a7a08d94c62cd5f76e67034bad4
Xie Yuheng <xyheme@gmail.com>
2024-11-03 04:48:32 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { createHead } from '@vueuse/head'
 import { defineCustomElement } from 'vue'
 import MimorComponent from './components/mimor/Mimor.ce.vue'
-
 export { mimorKeywords } from './components/mimor/mimorKeywords'
 
-export const Mimor = defineCustomElement(MimorComponent, {
-  configureApp(app) {
-    app.use(createHead())
-  },
-})
+export const Mimor = defineCustomElement(MimorComponent)
 
 customElements.define('x-mimor', Mimor)
