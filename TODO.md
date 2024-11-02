a8502f1361b831554be6c8531ecb92f32f23d8b4
Xie Yuheng <xyheme@gmail.com>
2024-11-03 05:27:06 +0800

    [layout] share 按钮

--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
-[playground] share 按钮
 [docs] mimor web component 使用文档
--- src/components/layout/Head.vue
+++ src/components/layout/Head.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline'
 import Lang from '../../components/lang/Lang.vue'
 import { useGlobalLang } from '../../components/lang/useGlobalLang'
 import { useGlobalTheme } from '../../models/theme'
@@ -6,6 +7,20 @@ import Logo from './Logo.vue'
 
 const lang = useGlobalLang()
 const theme = useGlobalTheme()
+
+const url = new URL(window.location.href)
+
+async function share() {
+  try {
+    await navigator.clipboard.writeText(window.location.href)
+    const message = lang.isZh()
+      ? `网址（URL）已复制到剪切板！`
+      : `URL copied to clipboard!`
+    window.alert(message)
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
 
 <template>
@@ -15,6 +30,17 @@ const theme = useGlobalTheme()
     </div>
 
     <div class="flex space-x-3">
+      <button
+        v-if="url.pathname.startsWith('/playground')"
+        class="flex items-center whitespace-nowrap hover:underline"
+        @click="share()"
+      >
+        <Lang>
+          <template #zh> 分享 </template>
+          <template #en> Share </template>
+        </Lang>
+      </button>
+
       <button
         class="flex items-center whitespace-nowrap hover:underline"
         @click="
@@ -46,12 +72,14 @@ const theme = useGlobalTheme()
       <a
         target="_blank"
         href="https://github.com/xieyuheng/mimor"
-        class="flex items-center whitespace-nowrap hover:underline"
+        class="flex items-center space-x-1 whitespace-nowrap hover:underline"
       >
         <Lang>
           <template #zh>源代码</template>
           <template #en>Source Code</template>
         </Lang>
+
+        <ArrowTopRightOnSquareIcon class="h-4 w-4" />
       </a>
     </div>
   </div>
